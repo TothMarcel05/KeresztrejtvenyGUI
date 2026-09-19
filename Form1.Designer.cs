e95b25e92f6416d1e6e2f8b612f661113a49fb88
TothMarcel05 <toth.marcel.daniel@diak.szbi-pg.hu>
2026-09-19 17:49:54 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace KeresztrejtvenyGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sor = new System.Windows.Forms.ComboBox();
+            this.oszlop = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.letrehozasButton = new System.Windows.Forms.Button();
+            this.mentesButton = new System.Windows.Forms.Button();
+            this.index = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // sor
+            // 
+            this.sor.FormattingEnabled = true;
+            this.sor.Location = new System.Drawing.Point(199, 35);
+            this.sor.Name = "sor";
+            this.sor.Size = new System.Drawing.Size(55, 21);
+            this.sor.TabIndex = 0;
+            // 
+            // oszlop
+            // 
+            this.oszlop.FormattingEnabled = true;
+            this.oszlop.Location = new System.Drawing.Point(260, 35);
+            this.oszlop.Name = "oszlop";
+            this.oszlop.Size = new System.Drawing.Size(55, 21);
+            this.oszlop.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Keresztrejtvény mérete [sor x oszlop]:";
+            // 
+            // letrehozasButton
+            // 
+            this.letrehozasButton.Location = new System.Drawing.Point(12, 77);
+            this.letrehozasButton.Name = "letrehozasButton";
+            this.letrehozasButton.Size = new System.Drawing.Size(142, 23);
+            this.letrehozasButton.TabIndex = 3;
+            this.letrehozasButton.Text = "Keresztrejtvény létrehozás";
+            this.letrehozasButton.UseVisualStyleBackColor = true;
+            this.letrehozasButton.Click += new System.EventHandler(this.letrehozasButton_Click);
+            // 
+            // mentesButton
+            // 
+            this.mentesButton.Location = new System.Drawing.Point(160, 77);
+            this.mentesButton.Name = "mentesButton";
+            this.mentesButton.Size = new System.Drawing.Size(142, 23);
+            this.mentesButton.TabIndex = 4;
+            this.mentesButton.Text = "Keresztrejtvény mentése";
+            this.mentesButton.UseVisualStyleBackColor = true;
+            this.mentesButton.Click += new System.EventHandler(this.mentesButton_Click);
+            // 
+            // index
+            // 
+            this.index.FormattingEnabled = true;
+            this.index.Location = new System.Drawing.Point(308, 79);
+            this.index.Name = "index";
+            this.index.Size = new System.Drawing.Size(55, 21);
+            this.index.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.Controls.Add(this.index);
+            this.Controls.Add(this.mentesButton);
+            this.Controls.Add(this.letrehozasButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.oszlop);
+            this.Controls.Add(this.sor);
+            this.Name = "Form1";
+            this.Text = "Keresztrejtvény készítő";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox sor;
+        private System.Windows.Forms.ComboBox oszlop;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button letrehozasButton;
+        private System.Windows.Forms.Button mentesButton;
+        private System.Windows.Forms.ComboBox index;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace KeresztrejtvenyGUI
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public static int sorok = 15;
+        public static int oszlopok = 15;
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //10. a.
+            for (int i = 6; i < 16; i++)
+            {
+                this.sor.Items.Add(i);
+                this.oszlop.Items.Add(i);
+            }
+            this.sor.SelectedItem = 15;
+            this.oszlop.SelectedItem = 15;
+
+            for (int i = 1; i < 11; i++)
+            {
+                this.index.Items.Add(i);
+            }
+            this.index.SelectedItem = 3;
+
+
+        }
+
+        private void letrehozasButton_Click(object sender, EventArgs e)
+        {
+            //10. b.
+            int x = 20;
+            int y = 90;
+
+            sorok = Convert.ToInt32(this.sor.SelectedItem);
+            oszlopok = Convert.ToInt32(this.oszlop.SelectedItem);
+
+            int height = this.Height - y - 10;
+            int width = this.Width - x * 2;
+
+            int tablaheight = sorok * 26;
+            int tablawidth = oszlopok * 26;
+
+            int a = (width - tablawidth) / 2;
+            int b = (height - tablaheight) / 2;
+
+
+
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    TextBox txt = new TextBox();
+                    txt.Location = new Point(a + j * 26, y + b + i * 26);
+                    txt.Size = new Size(25, 25);
+                    txt.Text = "-";
+                    txt.Name = $"txt_{i}_{j}";
+
+                    txt.MaxLength = 1; 
+                    txt.TextAlign = HorizontalAlignment.Center;
+
+
+
+                    txt.DoubleClick += textboxChange_DoubleClick;
+                    txt.Click += textboxChange_Click;
+                    Controls.Add(txt);
+                }
+            }
+
+
+        }
+        private void textboxChange_DoubleClick(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            if (txt.Text == "-") txt.Text = "#";
+            else txt.Text = "-";
+        }
+
+        private void textboxChange_Click(object sender, EventArgs e)
+        {
+            TextBox txt = (TextBox)sender;
+            
+        }
+
+        private void mentesButton_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter($"kr{index.SelectedItem}.txt");
+            for (int i = 0; i < sorok; i++)
+            {
+                for (int j = 0; j < oszlopok; j++)
+                {
+                    TextBox txt = (TextBox)this.Controls.Find($"txt_{i}_{j}", true)[0];
+                    string text = txt.Text;
+                    File.AppendAllText($"kr{index.SelectedItem}.txt", text);
+                }
+                File.AppendAllText($"kr{index.SelectedItem}.txt", Environment.NewLine);
+            }
+
+            MessageBox.Show("A keresztrejtvény mentése sikeres!");
+        }
+    }
+}
